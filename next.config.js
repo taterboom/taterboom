873e59c54f89aac17970453bc2d2b16591a6c570
xueyong <1085523029@qq.com>
2022-03-13 11:29:28 +0800

    chore: init

--- next.config.js
+++ next.config.js
@@ -3,4 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+const withNextra = require("nextra")({
+  theme: "nextra-theme-blog",
+  themeConfig: "./theme.config.js",
+  // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
+})
+
+module.exports = withNextra(nextConfig)
---
+++ pages/props-break.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from "next"
+
+const PropsBreak: NextPage = () => {
+  return (
+    <iframe
+      className="min-h-screen"
+      width="100%"
+      height="100%"
+      src="https://props-break.vercel.app/"
+    ></iframe>
+  )
+}
+
+export default PropsBreak
